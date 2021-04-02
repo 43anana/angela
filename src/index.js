942c18f33d14834c20a08b3c519c4e9a8f179c6a
43anana <75275624+43anana@users.noreply.github.com>
2021-04-02 15:34:14 +0200

    react first steps

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//var React = require("react");
+import React from "react";
+//var ReactDOM = require("react-dom");
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>hello worldi</h1>
+    <p>partypeople</p>
+  </div>,
+  document.getElementById("root")
+);
